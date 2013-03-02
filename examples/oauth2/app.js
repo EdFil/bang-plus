18fa0301ff9fd30bf8402efb55d345a0aacb2253
Edgar Santos <edfil221@gmail.com>
2013-03-02 05:48:19 +0000

    fsdfs

--- examples/oauth2/app.js
+++ examples/oauth2/app.js
@@ -120,11 +120,11 @@ app.get('/auth/google',
 //   which, in this example, will redirect the user to the home page.
 app.get('/oauth2callback',
   passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
   var xixi;
   for(coiso in res)
     xixi += coiso ' - ';
   console.log(xixi);
-  function(req, res) {
     res.redirect('/');
   });
 
